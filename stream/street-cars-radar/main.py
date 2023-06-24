ae9c139c602af9200ef4557cfee53ea15558ae10
rbojan2000 <rbojan2000@gmail.com>
2023-06-24 18:13:57 +0200

    test

--- stream/street-cars-radar/main.py
+++ stream/street-cars-radar/main.py
@@ -2,7 +2,7 @@ from producer.producer import produce_message
 from state_stores.geo import get_previous_and_next_node, calculate_traffix_congestion
 from utils.traffic_message_parser import parse_trafic_input_stream
 
-# L R T 
+# L R
 
 while True:
     message = produce_message()
